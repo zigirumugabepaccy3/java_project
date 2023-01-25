fba2a0d190da0ecc70fa06f023dcc3887fb29b8d
zigirumugabepaccy3 <121093859+zigirumugabepaccy3@users.noreply.github.com>
2023-01-26 00:32:47 +0200

    Add files via upload

---
+++ src/student/information/system/DbUtils.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student.information.system;
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author Hyrex
+ */
+class DbUtils {
+
+    static TableModel resultISetToTableModel(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static TableModel resultSetToTableModel(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
